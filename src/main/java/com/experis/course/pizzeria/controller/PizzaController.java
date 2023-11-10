1c10fdeb42736db81e22f3c9918287f30bc11d77
Bufal8 <125837797+Bufal8@users.noreply.github.com>
2023-11-10 15:18:46 +0100

    Created a detail page for pizzas + backend searchbar. Edited controller + repository

--- src/main/java/com/experis/course/pizzeria/controller/PizzaController.java
+++ src/main/java/com/experis/course/pizzeria/controller/PizzaController.java
@@ -3,12 +3,17 @@ package com.experis.course.pizzeria.controller;
 import com.experis.course.pizzeria.model.Pizza;
 import com.experis.course.pizzeria.repository.PizzaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/pizzas")
@@ -17,9 +22,20 @@ public class PizzaController {
     private PizzaRepository pizzaRepository;
 
     @GetMapping
-    public String index(Model model) {
-        List<Pizza> pizzaList = pizzaRepository.findAll();
-        model.addAttribute("pizzaList", pizzaList);
-        return "pizza/index";
+    public String index(@RequestParam Optional<String> search, Model model) {
+        List<Pizza> pizzaList; // This attribute is used to store the list of pizzas
+        if (search.isPresent()) {
+            pizzaList = pizzaRepository.findByNameContainingIgnoreCase(search.get()); // This method returns a list of pizzas that match the name passed as a parameter
+        } else {
+            pizzaList = pizzaRepository.findAll(); // This method returns all the pizzas in the database thanks to the pizzaRepository attribute
+        }
+        model.addAttribute("pizzaList", pizzaList); // This method adds the pizzas attribute to the model
+        return "pizza/list";
+    }
+
+    @GetMapping("/show/{id}")
+    public String show(@PathVariable Integer id, Model model) {
+        model.addAttribute("pizza", pizzaRepository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "pizza with id " + id + " not found"))); // This method adds the pizza attribute to the model
+        return "pizza/show";
     }
 }
--- src/main/java/com/experis/course/pizzeria/repository/PizzaRepository.java
+++ src/main/java/com/experis/course/pizzeria/repository/PizzaRepository.java
@@ -3,6 +3,9 @@ package com.experis.course.pizzeria.repository;
 import com.experis.course.pizzeria.model.Pizza;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PizzaRepository extends JpaRepository<Pizza, Integer>{
+import java.util.List;
 
+public interface PizzaRepository extends JpaRepository<Pizza, Integer>{
+    // Here you can define custom queries
+    List<Pizza> findByNameContainingIgnoreCase(String nameKeyword); // This method returns a list of pizzas that match the name passed as a parameter
 }
