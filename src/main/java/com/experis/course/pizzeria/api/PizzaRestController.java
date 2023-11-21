00e6176df48aa76541b5a2ee698dbd4beeb7de60
Bufal8 <125837797+Bufal8@users.noreply.github.com>
2023-11-21 17:38:40 +0100

    Converting into API application: get pizza's detail.

--- src/main/java/com/experis/course/pizzeria/api/PizzaRestController.java
+++ src/main/java/com/experis/course/pizzeria/api/PizzaRestController.java
@@ -1,9 +1,12 @@
 package com.experis.course.pizzeria.api;
 
+import com.experis.course.pizzeria.exceptions.PizzaNotFoundException;
 import com.experis.course.pizzeria.model.Pizza;
 import com.experis.course.pizzeria.service.PizzaService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 import java.util.Optional;
@@ -20,5 +23,16 @@ public class PizzaRestController {
     public List<Pizza> index(@RequestParam Optional<String> search) {
         // ritorna la lista delle pizze dal database
         return pizzaService.getPizzaList(search);
+    }
+
+    // dettaglio di una pizza
+    @GetMapping("/{id}")
+    public Pizza details(@PathVariable Integer id) {
+        // ritorna la pizza con id specificato
+        try {
+            return pizzaService.getPizzaById(id);
+        } catch (PizzaNotFoundException e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, e.getMessage());
+        }
     }
 }
